deb80d3c9d304ee4db7fb21a4e2605ce6ec960cb
LeeHyungGeun <lhg4031@gmail.com>
2016-03-14 20:23:36 +0800

    add og tag npm module

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express'),
     app = express(),
+    OG = require('express-metatag')('og'),
     port = process.env.PORT || 8000;
 
+app.use(OG([{
+  url: 'https://www.worksmobile.com/jp',
+  type: 'website',
+  title: '仕事がはかどる 気持ちが伝わる - Works Mobile',
+  description: 'いつでもどこでも、アプリひとつでメッセンジャーもメールもカレンダーが使えて仕事がはかどる。LINEスタンプで気持ちが伝わるビジネスコミュニケーションを。',
+  image: 'https://www.worksmobile.com/img/ogtag/ogtag_jp.jpg',
+  locale: 'ja_JP'
+}]));
 
 app.get('/', function (req, res) {
   // console.log(req.headers);
   console.log(req);
-  res.send('Hello World!');
-
+  res.end();
 });
 
 app.listen(port, function () {
